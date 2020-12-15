c4a17700fada5e6f6f677520732505dd62b5188c
Pawel Zmiejko <pzmiejko@gmail.com>
2020-12-15 21:18:36 +0100

    Modified health check

--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
 )
 
 const (
-	version = "0.1"
+	version = "0.1.0"
 )
 
 var (
 	conMeta   meta
 	counter   int
-	imReady   bool = false
+	initdelay = map[string]float32{
+		"zero":     0.0,
+		"bolt":     9.58,
+		"veyron":   16.7,
+		"lovejava": 50.0,
+	}
+	crashChance = map[string]float32{
+		"lotto":      float32(1.0 / 49.0),
+		"coin":       0.5,
+		"badluck":    0.9,
+		"invincible": 1,
+	}
+	// reverse chance - if true container will survive instead of crash with the defined probability
+	reverseChance bool = false
+	imReady       bool = false
 )
 
 type meta struct {
@@ -28,7 +50,7 @@ func (m *meta) newMeta() {
 	m.Hostname, _ = os.Hostname()
 }
 
-func (m *meta) getIP() {
+func (m *meta) getIp() {
 	ifaces, err := net.Interfaces()
 	if err != nil {
 		fmt.Printf("Error getting interfaces: %s", err)
@@ -59,15 +81,54 @@ func (m *meta) getIP() {
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	if imReady {
 		w.WriteHeader(200)
-		fmt.Fprintf(w, "OK")
+		fmt.Fprintf(w, "ok")
 	} else {
 		w.WriteHeader(503)
-		fmt.Fprintf(w, "Not ready")
+		fmt.Fprintf(w, "not ready")
+	}
+}
+
+func getReady(delayType string) {
+	if d, ok := initdelay[delayType]; ok {
+		fmt.Printf("Preparing (delay type='%s', duration=%.2fs)\n", delayType, d)
+		time.Sleep(time.Duration(d) * time.Second)
+		imReady = true
+		fmt.Printf("Ready!\n")
+	} else {
+		panic(fmt.Sprintf("Invalid delay type: %s", delayType))
+	}
+}
+
+func crashMe(crashChanceType string) {
+	chance := crashChance[crashChanceType]
+	// i := 0
+	for {
+		if !imReady {
+			time.Sleep(time.Second)
+		} else {
+			// shoot!
+			src := rand.NewSource(time.Now().UnixNano())
+			rnd := rand.New(src)
+			r := rnd.Float32()
+			c := chance
+			if reverseChance {
+				c = 1 - chance
+			}
+			if r < c {
+				panic(fmt.Sprintf("Killed! (drawn=%f, chance=%s, probability=%f)\n", r, crashChanceType, c))
+			} else {
+				fmt.Printf("Missed - lucky you :-) (drawn=%f, chance=%s, probability=%f)\n", r, crashChanceType, c)
+				return
+				// i += 1
+				// time.Sleep(20 * time.Millisecond)
+				// fmt.Printf("[%d] ", i)
+			}
+		}
 	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	counter++
+	counter += 1
 	fmt.Fprintf(w, "%-15s %s\n", "Hostname:", conMeta.Hostname)
 	fmt.Fprintf(w, "%-15s %s\n", "IP:", conMeta.IP)
 	fmt.Fprintf(w, "%-15s %s\n", "External IP:", conMeta.ExtIP)
@@ -75,21 +136,58 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%-15s %s\n", "Version:", version)
 }
 
-/*
 func jsonHandler(w http.ResponseWriter, r *http.Request) {
 	counter += 1
 	cj, _ := json.Marshal(conMeta)
 	fmt.Fprintf(w, "%s", string(cj))
 }
-*/
+
+func configFromEnv(key string) (string, error) {
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		if strings.ToLower(pair[0]) == key {
+			return pair[1], nil
+		}
+	}
+	return "", errors.New(fmt.Sprintf("Key %s not found in environment variables", key))
+}
+
+func sigHandler(sigs chan os.Signal, done chan bool) {
+	sig := <-sigs
+	fmt.Printf("Received and handling signal %s\n", sig)
+	done <- true
+	os.Exit(0)
+}
 
 func main() {
 	conMeta.newMeta()
-	conMeta.getIP()
+	conMeta.getIp()
 	counter = 0
 
-	http.HandleFunc("/healthz", healthHandler)
+	delay, err1 := configFromEnv("delay")
+	if err1 != nil {
+		flag.StringVar(&delay, "delay", "bolt", fmt.Sprintf("delay type: zero, bolt, veyron, lovejava"))
+	}
+	crash, err2 := configFromEnv("crash")
+	if err2 != nil {
+		flag.StringVar(&crash, "crash", "lotto", fmt.Sprintf("crash type: lotto, coin, badluck, invincible"))
+	}
+
+	flag.BoolVar(&reverseChance, "reverse", reverseChance, fmt.Sprintf("reverse action - survive instead of crash with defined probability"))
+
+	flag.Parse()
+	go getReady(delay)
+	go crashMe(crash)
+
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+
+	go sigHandler(sigs, done)
+
+	http.HandleFunc("/health", healthHandler)
 	http.HandleFunc("/", handler)
 	// http.HandleFunc("/json", jsonHandler)
-	log.Fatal(http.ListenAndServe(":8089", nil))
+	log.Fatal(http.ListenAndServe(":8088", nil))
 }
